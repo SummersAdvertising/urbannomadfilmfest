aa4e63c58011ff2f9a11e9886c333e904d20ffac
Yuzhe <yuzhe.c85@gmail.com>
2013-03-26 17:10:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urbannomadfilmfest::Application.config.secret_token = '1adddc5f05d4ca7febbdde26ffef1bc88c6177cb3db8cdf4b90bc6ecfb514d854fd2f38f3f100d713af60d78eb8ebc485874a52762b85a2a398c295966b7c827'
